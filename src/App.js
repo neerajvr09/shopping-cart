bd930f4b2ba033d87bf1975aaa95eaeae43b1b0f
Neeraj verma <74642854+neerajvr09@users.noreply.github.com>
2022-08-30 16:50:23 +0530

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
+import {Container,Row,Col} from "reactstrap"
+import BuySection from './Componnets/BuySection';
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+
+  const [cartItem, setCartItem] = useState([])
+
+  const addToCart = (item) => {
+    setCartItem([...cartItem, item])
+  }
+
+  const buyNow = () => {
+    setCartItem([])
+    toast("Purchase completed", {type:"success"})
+  }
+
+  const removeItem = (item) => {
+    setCartItem(
+      cartItem.filter(singleItem => {
+        return singleItem.id !== item.id
+      })
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <ToastContainer />
+       <Row>
+        <Col md={8}>
+          <BuySection addToCart={addToCart} />
+        </Col>
+        <Col md={4}></Col>
+       </Row>
+    </Container>
   );
 }
 
