89c1b54438f4998a027fe168d69858958fd99a10
Neeraj verma <74642854+neerajvr09@users.noreply.github.com>
2022-09-01 02:41:41 +0530

    added addToCart functionality

--- src/Componnets/CartItem.js
+++ src/Componnets/CartItem.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import {Card,CardImg,CardBody,CardTitle,CardText,Button} from "reactstrap"
 
-const CartItem = ({product}) => {
+const CartItem = ({product, addToCart}) => {
   return (
     <Card className='mt-2 mb-1'>
       <CardImg top height="250" width="100%" src={product.smallImage} />
       <CardBody className='text-center'>
         <CardTitle>{product.productName}</CardTitle>
         <CardText className="secondary">Price Rs. {product.productPrice} </CardText>
-        <Button color="success">Add</Button>
+        <Button color="success" onClick={() => addToCart(product)}>Add</Button>
       </CardBody>
     </Card>
   )
