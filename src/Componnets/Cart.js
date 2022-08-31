c8479d2cfae2f67d67700b825e88f384def9776f
Neeraj verma <74642854+neerajvr09@users.noreply.github.com>
2022-09-01 03:03:00 +0530

    created cart section

--- src/Componnets/Cart.js
+++ src/Componnets/Cart.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import {
+  Container,
+  ListGroup,
+  ListGroupItem,
+  Button,
+  CardHeader,
+  CardBody,
+  Card,
+  CardFooter,
+  Row,
+  Col,
+} from "reactstrap";
 
-const Cart = ()  => {
+const Cart = ({ cartItem, buyNow, removeItem }) => {
   return (
-    <div>Cart</div>
-  )
-}
+    <Container fluid>
+      <h1 className="text-success">Your Cart</h1>
+      <ListGroup>
+        {cartItem.map((item) => (
+          <ListGroupItem key={item.id}>
+            <Row>
+                <Col>
+                <img src={item.tinyImage} height="80" alt="image" />
+                </Col>
+                <Col className="text-center">
+                    <div className="text-primary">{item.productName}</div>
+                    <div>
+                        Price : {item.productPrice}
+                    <Button color="danger" onClick={() => removeItem(item)}>Remove</Button>
+                    </div>
+                </Col>
+            </Row>
+          </ListGroupItem>
+        ))}
+      </ListGroup>
 
-export default Cart
+      {
+        cartItem.length >=1 ? (
+            <Card className="text-center mt-3">
+                <CardHeader>Grand Total </CardHeader>
+                <CardBody>
+                    Your amount for {cartItem.length} product is : {}
+                </CardBody>
+                <CardFooter>
+                    <Button onClick={buyNow} color="success" >
+                        Buy
+                    </Button>
+                </CardFooter>
+            </Card>
+        ) : (
+            <h1 className="text-warning">Cart is empty</h1>
+        )
+      }
+    </Container>
+  );
+};
+
+export default Cart;
