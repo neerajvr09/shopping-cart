b9a2aa667893e0c9975c525c62835abfcba08930
Neeraj verma <74642854+neerajvr09@users.noreply.github.com>
2022-09-01 02:43:59 +0530

    integrated cart section

--- src/App.js
+++ src/App.js
@@ -1,43 +1,45 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import './App.css';
-import {Container,Row,Col} from "reactstrap"
-import BuySection from './Componnets/BuySection';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Container, Row, Col } from "reactstrap";
+import BuySection from "./Componnets/BuySection";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer, toast } from "react-toastify";
 import { useState } from "react";
+import Cart from "./Componnets/Cart";
 
 const App = () => {
-
-  const [cartItem, setCartItem] = useState([])
+  const [cartItem, setCartItem] = useState([]);
 
   const addToCart = (item) => {
-    setCartItem([...cartItem, item])
-  }
+    setCartItem([...cartItem, item]);
+  };
 
   const buyNow = () => {
-    setCartItem([])
-    toast("Purchase completed", {type:"success"})
-  }
+    setCartItem([]);
+    toast("Purchase completed", { type: "success" });
+  };
 
   const removeItem = (item) => {
     setCartItem(
-      cartItem.filter(singleItem => {
-        return singleItem.id !== item.id
+      cartItem.filter((singleItem) => {
+        return singleItem.id !== item.id;
       })
-    )
-  }
+    );
+  };
 
   return (
     <Container fluid>
       <ToastContainer />
-       <Row>
+      <Row>
         <Col md={8}>
           <BuySection addToCart={addToCart} />
         </Col>
-        <Col md={4}></Col>
-       </Row>
+        <Col md={4}>
+          <Cart buyNow={buyNow} removeItem={removeItem} />
+        </Col>
+      </Row>
     </Container>
   );
-}
+};
 
 export default App;
